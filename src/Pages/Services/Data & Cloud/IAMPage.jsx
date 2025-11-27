0b00a88977c2099c3b6bf8def79e6e96acc5993e
dhilip143 <kdhilip146@gmail.com>
2025-11-27 11:21:17 +0530

    okokok

--- src/Pages/Services/Data & Cloud/IAMPage.jsx
+++ src/Pages/Services/Data & Cloud/IAMPage.jsx
@@ -6,17 +6,17 @@ import IAMHero from "/src/Components/ServiceComponents/Data&Cloud/IAMPage/IAMHer
 
 import WhyChoose from "/src/Components/ServiceComponents/Data&Cloud/IAMPage/IAMChooseUs.jsx";
 import IAMBenefits from "/src/Components/ServiceComponents/Data&Cloud/IAMPage/IAMBenefit.jsx";
-import Whatiam from "./Components/ServiceComponents/Data&Cloud/IAMPage/whatiam.jsx";
+import Whatiam from "./Components/ServiceComponents/Data&Cloud/IAMPage/Whatiam.jsx";
 
 
 const IAMPage = () => {
- 
+
 
   return (
     <div className="bg-black relative overflow-hidden">
       <SparkleBg count={20} speed={0.8} color="253, 199, 0" />
       <IAMHero />
-       <Whatiam/>    
+      <Whatiam />
       <WhyChoose />
       <IAMBenefits />
     </div>
